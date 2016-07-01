88232dcf8a4517464a2ee35bfe02233447223fbb
Jon Wilson <jonwilsoncomedy@gmail.com>
2016-07-01 00:45:36 -0500

    Ajax working, appending to DOM, no styles yet

--- app.js
+++ app.js
@@ -13,7 +13,43 @@ whatAboutBob.then(function(data) {
   var bobTitle = data.Title;
   var $bobMovieTitle = $('<li>' + bobTitle + '</li>');
   $('.whatAboutBob').after($bobMovieTitle);
+  var bobYear = data.Year;
+  var $bobMovieYear = $('<li>' + bobYear + '</li>');
+  $('.whatAboutBob').after($bobMovieYear);
+  var bobPoster = data.Poster;
+  var $bobMoviePoster = $('<img src="' + bobPoster + '"</img>');
+  $('.whatAboutBob').after($bobMoviePoster);
 });
+
+var hamlet = $.get('http://www.omdbapi.com/?t=Hamlet&y=&plot=short&r=json');
+
+hamlet.then(function(data) {
+  var hamletTitle = data.Title;
+  var $hamletMovieTitle = $('<li>' + hamletTitle + '</li>');
+  $('.hamlet').after($hamletMovieTitle);
+  var hamletYear = data.Year;
+  var $hamletMovieYear = $('<li>' + hamletYear + '</li>');
+  $('.hamlet').after($hamletMovieYear);
+  var hamletPoster = data.Poster;
+  var $hamletMoviePoster = $('<img src="' + hamletPoster + '"</img>');
+  $('.hamlet').after($hamletMoviePoster);
+});
+
+var weepahWay = $.get('http://www.omdbapi.com/?t=Weepah%20Way%20For%20Now&y=&plot=short&r=json');
+
+weepahWay.then(function(data) {
+  var weepahWayTitle = data.Title;
+  var $weepahWayMovieTitle = $('<li>' + weepahWayTitle + '</li>');
+  $('.weepahWay').after($weepahWayMovieTitle);
+  var weepahWayYear = data.Year;
+  var $weepahWayMovieYear = $('<li>' + weepahWayYear + '</li>');
+  $('.weepahWay').after($weepahWayMovieYear);
+  var weepahWayPoster = data.Poster;
+  var $weepahWayMoviePoster = $('<img src="' + weepahWayPoster + '"</img>');
+  $('.weepahWay').after($weepahWayMoviePoster);
+});
+
+
 
 
 
